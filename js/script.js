fdb6bad676671ef83f377ae63106f2b31bcbcd2e
MatteoFilippini <matteofilippini8@gmail.com>
2022-01-24 14:24:25 +0100

    created function random number

--- js/script.js
+++ js/script.js
@@ -4,3 +4,18 @@ Dall'ok  parte un timer di 30 secondi. (ricordate che l'alert blocca il flusso. 
 Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite prompt().
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri  sono stati indovinati dall'utente.
 */
+
+
+// FUNZIONE NUMERO RANDOM
+const getRandomNumebers = () => {
+    const r = Math.floor(Math.random() * 10) + 1;
+    return r;
+}
+
+// GENREO 5 NUMERI RANDOM
+for (let i = 0; i < 5; i++) {
+    console.log(getRandomNumebers());
+}
+
+
+
